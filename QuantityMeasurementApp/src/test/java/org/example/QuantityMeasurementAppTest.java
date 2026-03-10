160a19f1b361c48868bbb8435d6d05999eb646a8
Garvit <gaurrit8@gmail.com>
2026-03-10 11:23:13 +0530

    [Garvit Gaur] Refactor . QuantityMeasurementApp to include Feet and Inches classes with equality checks and corresponding unit tests

--- QuantityMeasurementApp/src/test/java/org/example/QuantityMeasurementAppTest.java
+++ QuantityMeasurementApp/src/test/java/org/example/QuantityMeasurementAppTest.java
@@ -1,9 +1,14 @@
 package org.example;
 
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.example.QuantityMeasurementApp.Feet;
+import org.example.QuantityMeasurementApp.Inches;
+
 public class QuantityMeasurementAppTest {
+
+    // ---------- FEET TESTS ----------
 
     @Test
     public void testFeetEquality_SameValue() {
@@ -45,5 +50,49 @@ public class QuantityMeasurementAppTest {
         Feet f1 = new Feet(1.0);
 
         assertTrue(f1.equals(f1));
+    }
+
+    // ---------- INCHES TESTS ----------
+
+    @Test
+    public void testInchesEquality_SameValue() {
+
+        Inches i1 = new Inches(1.0);
+        Inches i2 = new Inches(1.0);
+
+        assertTrue(i1.equals(i2));
+    }
+
+    @Test
+    public void testInchesEquality_DifferentValue() {
+
+        Inches i1 = new Inches(1.0);
+        Inches i2 = new Inches(2.0);
+
+        assertFalse(i1.equals(i2));
+    }
+
+    @Test
+    public void testInchesEquality_NullComparison() {
+
+        Inches i1 = new Inches(1.0);
+
+        assertFalse(i1.equals(null));
+    }
+
+    @Test
+    public void testInchesEquality_DifferentClass() {
+
+        Inches i1 = new Inches(1.0);
+
+        assertFalse(i1.equals("test"));
+    }
+
+    @Test
+    public void testInchesEquality_SameReference() {
+
+        Inches i1 = new Inches(1.0);
+
+        assertTrue(i1.equals(i1));
     }
 }
