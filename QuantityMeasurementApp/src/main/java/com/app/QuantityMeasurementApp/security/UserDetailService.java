8b5cf05d5670a173120f58c5f12e0b6bd0b7775d
Garvit <gaurrit8@gmail.com>
2026-03-31 11:33:58 +0530

    [Garvit Gaur] Refactor . Implement Spring Data JPA repository for quantity measurements and user management

---
+++ QuantityMeasurementApp/src/main/java/com/app/QuantityMeasurementApp/security/UserDetailService.java
@@ -0,0 +1,24 @@
+package com.app.QuantityMeasurementApp.security;
+
+import com.app.QuantityMeasurementApp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        return userRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with username: " + username)
+                );
+    }
+}
